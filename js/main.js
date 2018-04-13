f97871578bc3cb52887141fa8026ac79d5820e50
davidli3100 <david.li3100@gmail.com>
2018-04-12 20:30:15 -0400

    finally made damn email forms work

--- js/main.js
+++ js/main.js
@@ -82,17 +82,17 @@ $(document).ready(function(){
     });      
     // -------   Mail Send ajax
 
-     $(document).ready(function() {
+ /*    $(document).ready(function() {
         var form = $('#myForm'); // contact form
         var submit = $('.submit-btn'); // submit button
         var alert = $('.alert-msg'); // alert div for show alert message
 
         // form submit event
-        form.on('submit', function(e) {
+    form.on('submit', function(e) {
             e.preventDefault(); // prevent default form submit
 
             $.ajax({
-                url: 'mail.php', // form action url
+                url: 'https://formspree.io/david.li3100@gmail.com', // form action url
                 type: 'POST', // form submit method get/post
                 dataType: 'html', // request type html/json/xml
                 data: form.serialize(), // serialize form data
@@ -109,8 +109,8 @@ $(document).ready(function(){
                     console.log(e)
                 }
             });
-        });
-    });
+        }); not needed for now
+    }); */
  });
 
  //submit paypal
@@ -144,4 +144,5 @@ $(document).ready(function(){
     $('.toggleSection').hide(1000);
     $('.messageSection').show(700);
  });
+
 
